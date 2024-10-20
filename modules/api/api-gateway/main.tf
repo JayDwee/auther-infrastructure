557991b3d3d1388f53d0c82e33e6fedcf031198c
Jack Draper <drapj002@gmail.com>
2024-10-21 00:13:17 +0100

    fix: add integration_http_method

--- modules/api/api-gateway/main.tf
+++ modules/api/api-gateway/main.tf
@@ -56,6 +56,7 @@ resource "aws_api_gateway_integration" "get_jwks_integration" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.jwks.id
   http_method = aws_api_gateway_method.get_jwks.http_method
+  integration_http_method = aws_api_gateway_method.get_jwks.http_method
   type        = "AWS"
   uri         = "arn:aws:apigateway:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:s3/${var.s3_bucket_name}/.well-known/jwks.json"
   credentials = aws_iam_role.s3access.arn
@@ -78,6 +79,7 @@ resource "aws_api_gateway_integration" "default" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.default.id
   http_method = aws_api_gateway_method.default.http_method
+  integration_http_method = aws_api_gateway_method.default.http_method
   type        = "AWS_PROXY"
   uri         = var.lambda_function_invoke_arn
 }
