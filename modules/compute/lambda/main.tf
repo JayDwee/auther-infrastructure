4ba48ca7984e8920b8bbc0b33fdcc926116201da
Jack Draper <drapj002@gmail.com>
2024-10-23 00:59:14 +0100

    fix: cloudwatch log policy

--- modules/compute/lambda/main.tf
+++ modules/compute/lambda/main.tf
@@ -40,7 +40,7 @@ data "aws_iam_policy_document" "cloudwatch_readwrite" {
     effect = "Allow"
     actions = ["logs:CreateLogStream", "logs:PutLogEvents",]
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.lambda_function.function_name}/*"
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.lambda_function.function_name}:*"
     ]
   }
 }
