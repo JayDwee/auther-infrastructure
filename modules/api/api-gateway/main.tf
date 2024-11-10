d6a57979bf55d400fb42fc964c1acdd7b86e6d9a
Jack Draper <drapj002@gmail.com>
2024-11-10 23:06:45 +0000

    fix: foreach mention

--- modules/api/api-gateway/main.tf
+++ modules/api/api-gateway/main.tf
@@ -191,7 +191,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = var.lambda_function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.lambda_proxy.http_method}${aws_api_gateway_resource.default.path}"
+  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/ANY/*"
 }
 
 resource "aws_api_gateway_deployment" "default" {
